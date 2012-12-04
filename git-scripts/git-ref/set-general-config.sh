30ad1268a8871958eb23f2cc91d40daf2f96a9b5
VC (a.k.a kOVC) <vc@korganization.com>
2012-12-05 00:06:44 +0200

    Resetting author, again.

--- git-scripts/git-ref/set-general-config.sh
+++ git-scripts/git-ref/set-general-config.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #The following information was extracted from:
 #https://help.github.com/articles/set-up-git
+#http://git-scm.com/book/en/
+#http://gitref.org/
 #Edit the needed information, save, then run "bash set-user-info.sh" on the 
 #terminal window
 #
@@ -40,3 +42,32 @@ git config --global user.email "your_email@youremail.com"
 #1.7.10 or later, comment the next few lines
 #Set the cache to timeout after 1 hour (setting is in seconds)
 git config --global credential.helper 'cache --timeout=3600'
+#______________________________________________________________________________
+#Turn on coloring in git output. This will enable a default coloring scheme
+#in all git output. This option is available since version 1.5.5. If you 
+#have an older version, you'll need to set the colors for each type of output
+#individually (look in the next section for how to do that). You can also set 
+#the color.ui to false, or to always, which is like true, but it will color
+#the output even if the output is not directed to the terminal
+git config --global color.ui true
+#______________________________________________________________________________
+#You can turn specific output coloring on and off too. This means that 
+#individual types of output can have different coloring options.
+#The output types that you can customize are:
+#
+#color.branch
+#color.diff
+#color.interactive
+#color.status
+#
+#all of which can be set to false, true or always.
+#Uncomment the next line if you want to use it, it's an example
+#git config --global color.branch always
+#
+#Also, all of those have subattributes that you can set for different
+#colors (normal, black, red, green, yellow, blue, magenta, cyan, or white)
+#background colors (same as colors) and styles (bold, dim, ul, blink, 
+#and reverse)
+#Uncomment the next line if you want to use it, it's an example
+#git config --global color.diff.meta "blue black bold"
+#
