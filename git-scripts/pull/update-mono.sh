897410079097379adabfe9ad75a7dc1212317e21
VC (a.k.a kOVC) <vc@korganization.com>
2012-12-23 19:57:28 +0200

    Added the update-gnu.sh shell script which pulls changes from the gnu repos. Also modified update-mono.sh

--- git-scripts/pull/update-mono.sh
+++ git-scripts/pull/update-mono.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
+#This script assumes you already cloned the mono repos using one of the clone scripts.
+#it relies on the repositories being available under a "~/git/mono/" directory.
+#The script pulls changes from the repos into their local clones.
+#This is useful if you don't expect to change anything in any of the repos. For
+#example, if you are simply mirroring the repos
+#
+# THIS IS A VERY TRIVIAL SCRIPT. It's not the best approach. I'll work on it later, but
+# for now, that's all there is.
+#
+###############################################################################
+# clone-gnu.sh
+#            A bash script to clone all git repository from GNU Savannah
+# Author:
+#      VC (aka kOVC) <vc at korganization dot com>
+#
+# Copyright (c) 2012 kOVC
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You can find a full copy of the GPL license in many formats in the root 
+# directory of the distribution containing this file under the name
+# COPYING, COPYING.dbk, COPYING.html, COPYING.odt, COPYING.dbk.tex, 
+# and COPYING.texi
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+###############################################################################
+#
 cd ~/git/mono/acceptance-tests
 git pull
 cd ~/git/mono/aspeditor
